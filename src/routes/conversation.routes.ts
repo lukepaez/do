85721291024160671c8eb575bbd9aa5e083b0855
Ben Sleeper <sleeperb.cs@gmail.com>
2023-11-11 20:08:01 -0500

    initial openai api connection

--- src/routes/conversation.routes.ts
+++ src/routes/conversation.routes.ts
@@ -1,3 +1,7 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+OPENAI_API_KEY = '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';
+
 export const conversationRoutes = (fastify: any, options: any, done: any) => {
     fastify.get('/conversation', (req: any, res: any) => {
         res.send({ status: 'healthy', route: '/conversation' });
@@ -5,3 +9,13 @@ export const conversationRoutes = (fastify: any, options: any, done: any) => {
 
     done();
 };
+
+const configuration = new Configuration({
+    organization: 'org-w5yYeVYCdUu1l1dijq8LArOW',
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines();
+
+console.log(response.data);
