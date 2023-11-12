74b05cc7612119c4a961465faf67580c85cb0ded
Ben Sleeper <sleeperb.cs@gmail.com>
2023-11-11 21:51:58 -0500

    successful response

--- src/routes/conversation.routes.ts
+++ src/routes/conversation.routes.ts
@@ -1,21 +1,38 @@
 import OpenAI from 'openai';
-import Configuration from 'openai';
 
 export const conversationRoutes = (fastify: any, options: any, done: any) => {
     fastify.get('/conversation', (req: any, res: any) => {
         res.send({ status: 'healthy', route: '/conversation' });
 
-        console.log(conversation());
+        const openai = new OpenAI({
+            apiKey: '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',
+            organization: 'org-w5yYeVYCdUu1l1dijq8LArOW',
+        });
+
+        const userInput = 'Hello yeepy yee!';
+
+        agent(userInput, openai);
     });
 
     done();
 };
 
-async function conversation() {
-    const chatCompletion = await OpenAI.Chat.Completions.Create({
-        messages: [{ role: 'user', content: 'Say this is a test' }],
-        model: 'gpt-3.5-turbo',
-    });
+const messages = [
+    {
+        role: 'system',
+        content:
+            'you are yeepy yee. A chatbot named GPT 3.5 Turbo 1106, with a nickname of yeepy yee.',
+    },
+];
 
-    return chatCompletion;
+async function agent(userInput: any, openai: any) {
+    messages.push({
+        role: 'user',
+        content: userInput,
+    });
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo-1106',
+        messages: messages,
+    });
+    console.dir(response.choices[0]);
 }
