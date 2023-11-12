ab21c659149f0502a8c0b6ccafca9c2959351ad3
Luke Paez <lukepaez@macbook-pro.lan>
2023-11-11 19:37:20 -0500

    init proj

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello from typescript');
+import Fastify from 'fastify';
+import { initRoutes } from './routes/init.routes';
+import { conversationRoutes } from './routes/conversation.routes';
+
+// build app
+export const register = () => {
+    const fastify = Fastify({
+        logger: true,
+    });
+
+    // init routes
+    fastify.register(initRoutes);
+
+    // conversation routes
+    fastify.register(conversationRoutes);
+    return fastify;
+};
