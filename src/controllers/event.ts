ce925006a260da0d01055af12f1198b1bdb3d7c2
Ben Sleeper <sleeperb.cs@gmail.com>
2023-11-13 21:27:17 -0500

    discord bot & event endpoint

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -1,22 +1,46 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { ChatCompletionsAPI } from '../services/openAI/chat.service';
 import { insertPrompt } from '../config/helper';
+import fs from 'fs';
 
 export const createEvent = async (req: FastifyRequest, res: FastifyReply) => {
-    // insert prompt
-    const messages = insertPrompt(req.body);
+    try {
+        // insert prompt
+        const messages = insertPrompt(req.body);
+        // eslint-disable-next-line no-console
+        const userIdObj = JSON.stringify(req.params);
+        // eslint-disable-next-line no-console
+        console.log(userIdObj);
 
-    // create event
-    const event = ChatCompletionsAPI(messages, 'gpt-4-1106-preview');
+        fs.writeFile('newUser.json', userIdObj, 'utf8', function (err) {
+            if (err) {
+                // eslint-disable-next-line no-console
+                console.log(
+                    'An error occurred while writing JSON Object to File.'
+                );
+                // eslint-disable-next-line no-console
+                return console.log(err);
+            }
+            // eslint-disable-next-line no-console
+            console.log('JSON file has been saved.');
+        });
 
-    // call gpt
-    const data = await event.chatCompletionsCreate();
+        // create event
+        const event = ChatCompletionsAPI(messages, 'gpt-4-1106-preview');
 
-    // eslint-disable-next-line no-console
-    console.log(data);
+        // call gpt
+        const data = await event.chatCompletionsCreate();
 
-    // return res
-    res.send(data);
+        // eslint-disable-next-line no-console
+        //console.log(data);
+
+        // return res
+        res.send(data);
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        return res.send(error);
+    }
 
     //gpt tp be called
     //provide gpt the event list
