f1a4f105e327249b08c52010eae0033bc74fd715
Luke Paez <lukepaez@lukes-mbp.lan>
2023-11-14 00:22:09 -0500

    lts

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -8,36 +8,32 @@ import { readFileSync, writeFileSync } from 'fs';
 import path from 'path';
 
 type Data = {
-    userId: string;
-    thread_id: string;
-    assistant_id: string;
+    [key: string]: string;
 };
+
+const threadMap: Data = {};
 
 export const createEvent = async (req: any, res: FastifyReply) => {
     try {
-        // create assistant
-        const assistant = await Assistants.createAssistant(
-            'You are a personal grammar coach. Evaluate my text and sugguest a one sentence critique on how to make it more gramatically correct.',
-            'Grammy',
-            [],
-            'gpt-4-1106-preview'
-        );
+        const user = await temp(req.params.userId);
+        console.log(req?.body.content);
 
-        console.log('\n My Assistant: ', assistant.id);
-
-        const thread = await Threads.createThread();
-
-        console.log('\n My Thread: ', thread);
+        if (user instanceof Error) {
+            return user;
+        }
 
         const message = await Messages.createMessage(
-            thread.id,
+            user[req.params.userId],
             'user',
             req?.body.content
         );
 
         console.log('\n My Message: ', message);
 
-        const run = await Runs.createRun(thread.id, assistant.id);
+        const run = await Runs.createRun(
+            user[req.params.userId],
+            process.env?.ASSISTANT_ID ? process.env.ASSISTANT_ID : ''
+        );
 
         console.log('\n My Run: ', run);
 
@@ -46,17 +42,27 @@ export const createEvent = async (req: any, res: FastifyReply) => {
                 setTimeout(resolve, 3000);
                 console.log('polling...');
             });
-            const runStatus = await Runs.retrieveRun(thread.id, run.id);
+            const runStatus = await Runs.retrieveRun(
+                user[req.params.userId],
+                run.id
+            );
             if (runStatus.status != 'completed') {
                 continue;
             }
             break;
         }
 
-        const messages = await Messages.listMessages(thread.id);
+        const messages = await Messages.listMessages(user[req.params.userId]);
+
+        console.log('\n My messages: ', messages);
 
         const response = messages.data
             .filter(msg => {
+                console.log(
+                    msg?.content[0].type === 'text'
+                        ? msg.content[0].text.value
+                        : null
+                );
                 return msg.run_id === run.id && msg.role === 'assistant';
             })
             .pop();
@@ -69,6 +75,7 @@ export const createEvent = async (req: any, res: FastifyReply) => {
         });
     } catch (error) {
         console.log(error);
+        return error;
     }
 };
 
@@ -131,4 +138,35 @@ export const insertUser = async (userId: string, content: string) => {
             }
         }
     }
+};
+
+export const temp = async (userId: string): Promise<Data | Error> => {
+    try {
+        console.log('\n My user: ', userId);
+        let openAiThreadId = threadMap[userId];
+
+        // no thread exists for user
+        if (!openAiThreadId) {
+            // create thread
+            const thread = await Threads.createThread();
+            openAiThreadId = thread.id;
+            addThreadToMap(userId, openAiThreadId);
+            return threadMap;
+        }
+
+        return threadMap;
+
+        // add messages to thread
+    } catch (error) {
+        console.log(error);
+        return new Error('bad');
+    }
+};
+
+const addThreadToMap = (userId: string, threadId: string) => {
+    return (threadMap[userId] = threadId);
+};
+
+const getThreadId = (userId: string) => {
+    return threadMap[userId];
 };
