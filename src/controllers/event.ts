eab8546b5e339d901817c1e7e375ac027f7d1a13
Ben Sleeper <sleeperb.cs@gmail.com>
2023-11-14 23:17:13 -0500

    json enforcement completions api

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -7,6 +7,7 @@ import { Messages } from '../services/openai/assistants/messages.service';
 import { Runs } from '../services/openai/assistants/runs.service';
 import { readFileSync, writeFileSync } from 'fs';
 import path from 'path';
+import { ChatCompletionsAPI } from '../services/openai/chat.service';
 
 type Data = {
     [key: string]: string;
@@ -62,28 +63,47 @@ export const createEvent = async (req: any, res: FastifyReply) => {
 
         console.log('\n My content: ', messages.data[0].content);
 
-        const response = messages.data
-            .filter(msg => {
-                console.log(
-                    msg?.content[0].type === 'text'
-                        ? msg.content[0].text.value
-                        : null
-                );
-                return msg.run_id === run.id && msg.role === 'assistant';
-            })
-            .pop();
-        res.send({
-            id:
-                response?.content[0].type === 'text'
-                    ? response.content[0].text.value
-                    : null,
-            index: 0,
-        });
+        //console.log('\n My content: ', messages.data[0].content[0]?.text.value);
+
+        const eventToJSON =
+            messages?.data[0]?.content[0].type === 'text'
+                ? messages?.data[0]?.content[0].text.value
+                : null;
+
+        const body = [
+            {
+                role: 'system',
+                content:
+                    "You are a json enforcer helping an ai assistant called DO clarify their responses. DO's goal is to examine past events entered by users and update a profile on the user at each event. DO needs help from the json enforcer(you) to make sure the output is in valid json format. Please examine this context, and enforce valid json strictly in this format: {response: 'response', profile: 'profile'}. In this first update, please do not change the 'response' or 'profile' fields, just enforce they are in json, in the specified format. ",
+            },
+            {
+                role: 'user',
+                content: JSON.stringify(eventToJSON),
+            },
+        ];
+
+        //console.log('response: ', response);
+
+        const chatObj = ChatCompletionsAPI(body, 'gpt-4-1106-preview');
+
+        const responseInJson = await chatObj.chatCompletionsCreate();
+
+        console.log('json mode response: ', responseInJson);
+
+        if (responseInJson.message.content === null) {
+            return new Error('bad gpt res');
+        }
+
+        const res = JSON.parse(responseInJson?.message?.content);
+        responseInJson.message.content = res;
+        return responseInJson;
     } catch (error) {
         console.log(error);
         return error;
     }
 };
+
+//export const completionsPass = () => {};
 
 // does user exist?
 // yes: does thread exist
